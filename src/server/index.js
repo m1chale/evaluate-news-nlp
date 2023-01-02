71851ff12f948df706c9fa0a3d5f6d7a54a6bcc1
Michael Friedrich <m.friedrich3@yahoo.com>
2023-01-02 13:03:17 +0100

    install all dependencies

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,9 @@
 var path = require("path");
 const express = require("express");
 const mockAPIResponse = require("./mockAPI.js");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
@@ -10,7 +13,6 @@ console.log(__dirname);
 
 app.get("/", function (req, res) {
   res.sendFile("dist/index.html");
-  //res.sendFile(path.resolve("src/client/views/index.html"));
 });
 
 // designates what port the app will listen to for incoming requests
