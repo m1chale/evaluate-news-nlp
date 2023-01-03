57e92b9d9ce80e1857020b207063f1ddcaa9b51d
Michael Friedrich <m.friedrich3@yahoo.com>
2023-01-03 14:24:29 +0100

    form input / pass to api

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,13 +2,12 @@ export function handleSubmit(event) {
   event.preventDefault();
 
   // check what text was put into the form field
-  let formText = document.getElementById("name").value;
-  Client.checkForName(formText);
+  let analyseText = document.getElementById("input-text").value;
 
-  console.log("::: Form Submitted :::");
+  //Client.checkForName(formText);
 
   const urlParams = new URLSearchParams({
-    text: formText,
+    text: analyseText,
   });
 
   fetch("http://localhost:8080/api/nlp/" + urlParams)
